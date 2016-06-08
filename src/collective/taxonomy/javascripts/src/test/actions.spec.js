5c9387b47f4c91bd73df70fdb479526b67a90c19
Cédric Messiant <cedricmessiant@ecreall.com>
2016-06-08 13:27:56 +0200

    Add tests for actions and reducers

---
+++ src/collective/taxonomy/javascripts/src/test/actions.spec.js
@@ -0,0 +1,45 @@
+import * as actions from '../js/actions'
+import * as constants from '../js/constants'
+
+describe('addNode action creator', () => {
+  it('should create an action to add a node', () => {
+    const { addNode } = actions
+    const actual = addNode('foo', 1, 'xyz', ['en', 'fr'])
+    const expected = {
+      type: constants.ADD_NODE,
+      parentId: 'foo',
+      index: 1,
+      newKey: 'xyz',
+      languages: ['en', 'fr']
+    }
+    expect(actual).to.eql(expected)
+  })
+})
+
+describe('removeNode action creator', () => {
+  const { removeNode } = actions
+  it('should create an action to remove a node', () => {
+    const actual = removeNode('foo', 'bar', 42)
+    const expected = {
+      type: constants.REMOVE_NODE,
+      parentId: 'foo',
+      id: 'bar',
+      index: 42,
+    }
+    expect(actual).to.eql(expected)
+  })
+})
+
+describe('editTranslation action creator', () => {
+  const { editTranslation } = actions
+  it('should create an action to edit a translation', () => {
+    const actual = editTranslation('foo', 'fr', 'Lorem ipsum')
+    const expected = {
+      type: constants.EDIT_TRANSLATION,
+      id: 'foo',
+      language: 'fr',
+      value: 'Lorem ipsum'
+    }
+    expect(actual).to.eql(expected)
+  })
+})
---
+++ src/collective/taxonomy/javascripts/src/test/setup.js
@@ -0,0 +1,12 @@
+import chai from 'chai'
+// import chaiImmutable from 'chai-immutable'
+import equalJSX from 'chai-equal-jsx'
+// import spies from 'chai-spies'
+
+// chai.use(chaiImmutable)
+chai.use(equalJSX)
+// chai.use(spies)
+
+global.__DEV__ = true
+global.expect = chai.expect
+// global.spy = chai.spy
