05f399110116392026f7e6f56fdd13661c07b043
Tom Gross <itconsense@gmail.com>
2016-09-25 08:53:23 +0200

    test on Plone 5.a and use egg cache on travis

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Framework :: Zope2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
