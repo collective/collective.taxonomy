940c7c933b3dd498747bb83e2adff3407ed38169
Vincent Fretin <vincent.fretin@gmail.com>
2016-11-03 16:00:13 +0100

    Back to development: 1.4.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*pathnames):
     fh = open(os.path.join(os.path.dirname(__file__), *pathnames))
     return fh.read().decode('utf-8')
 
-version = '1.4.0'
+version = '1.4.1.dev0'
 
 setup(
     name='collective.taxonomy',
