31b9b95b6c2f803eb5592d6d88ebe7ea973c165e
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2020-07-01 04:43:00 -0300

    Refactor test

--- src/collective/taxonomy/javascripts/cypress/integration/end_to_end.js
+++ src/collective/taxonomy/javascripts/cypress/integration/end_to_end.js
@@ -2,12 +2,7 @@
 
 context('Navigation', () => {
   before(() => {
-    cy.visit('/');
-    cy.get('#personaltools-login').click();
-    cy.get('#__ac_name').type('admin');
-    cy.get('#__ac_password').type('admin');
-    cy.get('.pattern-modal-buttons > #buttons-login').click();
-    cy.contains('admin').should('exist');
+    cy.login();
   })
 
   beforeEach(() => {
@@ -26,9 +21,10 @@ context('Navigation', () => {
 
   it('Add taxonomy data', () => {
     cy.visit('/@@taxonomy-settings');
-    cy.get('#form-widgets-taxonomies-0').click();
+    cy.get('#form-widgets-taxonomies-0').check();
     cy.get('#form-buttons-edit_data_taxonomy').click();
     cy.get('.tree-view_children > button').click();
+
     cy.get('.input-field > input').type('Museums');
     cy.get('button[title="Add a term inside this node"]:last').click();
     cy.get('.input-field > input:last').type('Netherlands');
@@ -36,6 +32,7 @@ context('Navigation', () => {
     cy.get('.input-field > input:last').type('France');
     cy.get('button[title="Add a node at the same level"]:last').click();
     cy.get('.input-field > input:last').type('England');
+
     cy.get('button[title="Add a node at the same level"]:first').click();
     cy.get('.input-field > input:last').type('Companies');
     cy.get('button[title="Add a term inside this node"]:last').click();
@@ -47,88 +44,73 @@ context('Navigation', () => {
   })
 
   it('Enable behavior', () => {
-    cy.visit('/@@dexterity-types/Document/@@behaviors');
-    cy.get('#form-widgets-collective-taxonomy-generated-category-0').click();
+    cy.visit('/@@dexterity-types/News%20Item/@@behaviors');
+    cy.get('#form-widgets-collective-taxonomy-generated-category-0').check();
     cy.get('#form-buttons-apply').click();
     cy.contains('Behaviors successfully updated.').should('exist');
   })
 
-  it('Add categorized pages', () => {
-    cy.visit('/++add++Document');
-    cy.get('#form-widgets-IDublinCore-title').type('Centraal Museum');
-    cy.get('#form-widgets-IDublinCore-description').type('https://www.centraalmuseum.nl/');
-    cy.get('#autotoc-item-autotoc-2').click();
-    cy.get('#form-widgets-category-taxonomy_category-from').select('Museums » Netherlands');
-    cy.get('button[value=↓]').click();
-    cy.get('#form-buttons-save').click();
-    cy.contains('Item created').should('exist');
+  it('Add folders', () => {
+    cy.addFolder('', 'Portifolio');
+    cy.addFolder('/portifolio', 'All museums');
+    cy.addFolder('/portifolio', 'Netherlands Museums');
+    cy.addFolder('/portifolio', 'France Museums');
+  })
 
-    cy.visit('/++add++Document');
-    cy.get('#form-widgets-IDublinCore-title').type('Zeeuws Museum');
-    cy.get('#form-widgets-IDublinCore-description').type('https://www.zeeuwsmuseum.nl/');
-    cy.get('#autotoc-item-autotoc-2').click();
-    cy.get('#form-widgets-category-taxonomy_category-from').select('Museums » Netherlands');
-    cy.get('button[value=↓]').click();
-    cy.get('#form-buttons-save').click();
-    cy.contains('Item created').should('exist');
-
-    cy.visit('/++add++Document');
-    cy.get('#form-widgets-IDublinCore-title').type('Kunsthal KAdE');
-    cy.get('#form-widgets-IDublinCore-description').type('http://www.kunsthalkade.nl/');
-    cy.get('#autotoc-item-autotoc-2').click();
-    cy.get('#form-widgets-category-taxonomy_category-from').select('Museums » Netherlands');
-    cy.get('button[value=↓]').click();
-    cy.get('#form-buttons-save').click();
-    cy.contains('Item created').should('exist');
-
-    cy.visit('/++add++Document');
-    cy.get('#form-widgets-IDublinCore-title').type('Louvre Museum');
-    cy.get('#form-widgets-IDublinCore-description').type('https://www.louvre.fr');
-    cy.get('#autotoc-item-autotoc-2').click();
-    cy.get('#form-widgets-category-taxonomy_category-from').select('Museums » France');
-    cy.get('button[value=↓]').click();
-    cy.get('#form-buttons-save').click();
-    cy.contains('Item created').should('exist');
+  it('Add categorized news items', () => {
+    cy.addNewsItem(
+      '/portifolio',
+      'Centraal Museum',
+      'https://www.centraalmuseum.nl/',
+      'Centraal_Museum.png',
+      'Museums » Netherlands'
+    );
+    cy.addNewsItem(
+      '/portifolio',
+      'Zeeuws Museum',
+      'https://www.zeeuwsmuseum.nl/',
+      'Zeeuws_Museum.png',
+      'Museums » Netherlands'
+    );
+    cy.addNewsItem(
+      '/portifolio',
+      'Kunsthal KAdE',
+      'http://www.kunsthalkade.nl/',
+      'Kunsthal_KAdE.png',
+      'Museums » Netherlands'
+    );
+    cy.addNewsItem(
+      '/portifolio',
+      'Louvre Museum',
+      'https://www.louvre.fr',
+      'Musee_du_Louvre.png',
+      'Museums » France'
+    );
   })
 
   it('Create collections filtering by categories', () => {
-    cy.visit('/++add++Collection');
-    cy.get('#form-widgets-IDublinCore-title').type('Netherlands Museums');
-    cy.get('#select2-chosen-12').click();
-    cy.get('.select2-result-label:last').click();
-    cy.get('ul.select2-choices:first').click();
-    cy.get('#select2-drop > ul > li:nth-child(7) > div').click();
-    cy.wait(1000);
-    cy.get('#form-buttons-save').click();
+    cy.addCollection('/portifolio', 'Portifolio', 4);
+    cy.contains('#content', 'Centraal Museum').should('exist');
+    cy.contains('#content', 'Zeeuws Museum').should('exist');
+    cy.contains('#content', 'Kunsthal KAdE').should('exist');
+    cy.contains('#content', 'Louvre Museum').should('exist');
+
+    cy.addCollection('/portifolio/all-museums', 'All Museums', 4);
+    cy.contains('#content', 'Centraal Museum').should('exist');
+    cy.contains('#content', 'Zeeuws Museum').should('exist');
+    cy.contains('#content', 'Kunsthal KAdE').should('exist');
+    cy.contains('#content', 'Louvre Museum').should('exist');
+
+    cy.addCollection('/portifolio/netherlands-museums', 'Netherlands Museums', 7);
     cy.contains('#content', 'Centraal Museum').should('exist');
     cy.contains('#content', 'Zeeuws Museum').should('exist');
     cy.contains('#content', 'Kunsthal KAdE').should('exist');
     cy.contains('#content', 'Louvre Museum').should('not.exist');
 
-    cy.visit('/++add++Collection');
-    cy.get('#form-widgets-IDublinCore-title').type('France Museums');
-    cy.get('#select2-chosen-12').click();
-    cy.get('.select2-result-label:last').click();
-    cy.get('ul.select2-choices:first').click();
-    cy.get('#select2-drop > ul > li:nth-child(6) > div').click();
-    cy.wait(1000);
-    cy.get('#form-buttons-save').click();
+    cy.addCollection('/portifolio/france-museums', 'France Museums', 6);
     cy.contains('#content', 'Centraal Museum').should('not.exist');
     cy.contains('#content', 'Zeeuws Museum').should('not.exist');
     cy.contains('#content', 'Kunsthal KAdE').should('not.exist');
-    cy.contains('#content', 'Louvre Museum').should('exist');
-
-    cy.visit('/++add++Collection');
-    cy.get('#form-widgets-IDublinCore-title').type('All Museums');
-    cy.get('#select2-chosen-12').click();
-    cy.get('.select2-result-label:last').click();
-    cy.get('ul.select2-choices:first').click();
-    cy.get('#select2-drop > ul > li:nth-child(4) > div').click();
-    cy.wait(1000);
-    cy.get('#form-buttons-save').click();
-    cy.contains('#content', 'Centraal Museum').should('exist');
-    cy.contains('#content', 'Zeeuws Museum').should('exist');
-    cy.contains('#content', 'Kunsthal KAdE').should('exist');
     cy.contains('#content', 'Louvre Museum').should('exist');
   })
 })
--- src/collective/taxonomy/javascripts/cypress/support/commands.js
+++ src/collective/taxonomy/javascripts/cypress/support/commands.js
@@ -23,3 +23,67 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/');
+  if (/__ac/.test(document.cookie)) {
+    return;
+  }
+  cy.get('#personaltools-login').click();
+  cy.get('#__ac_name').type('admin');
+  cy.get('#__ac_password').type('admin');
+  cy.get('.pattern-modal-buttons > #buttons-login').click();
+  cy.contains('admin').should('exist');
+});
+
+Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fileName, fileType = '') => {
+  cy.fixture(fileName,'binary').then(content => {
+    return Cypress.Blob.binaryStringToBlob(content, fileType).then(blob => {
+      const el = subject[0];
+      const testFile = new File([blob], fileName, {type: fileType});
+      const dataTransfer = new DataTransfer();
+
+      dataTransfer.items.add(testFile);
+      el.files = dataTransfer.files;
+      cy.wrap(subject).trigger('change', { force: true });
+    });
+  });
+});
+
+Cypress.Commands.add('saveAndPublish', () => {
+  cy.get('#form-buttons-save').click();
+  cy.contains('Item created').should('exist');
+  cy.get('.label-state-private').click();
+  cy.get('#workflow-transition-publish').click();
+  cy.contains('Item state changed.').should('exist');
+});
+
+Cypress.Commands.add('addFolder', (path, title) => {
+    cy.visit(`${path}/++add++Folder`);
+    cy.get('#form-widgets-IDublinCore-title').type(title);
+    cy.saveAndPublish();
+});
+
+Cypress.Commands.add('addNewsItem', (path, title, site, image, category) => {
+  cy.visit(`${path}/++add++News%20Item`);
+  cy.get('#form-widgets-IDublinCore-title').type(title);
+  cy.get('#form-widgets-IDublinCore-description').type(site);
+  cy.get('input[type=file]').uploadFile(image, 'image/png');
+  cy.get('#autotoc-item-autotoc-1').click();
+  cy.get('#form-widgets-category-taxonomy_category-from').select(category);
+  cy.get('button[value=↓]').click();
+  cy.saveAndPublish();
+});
+
+Cypress.Commands.add('addCollection', (path, title, index) => {
+  cy.visit(`${path}/++add++Collection`);
+  cy.get('#form-widgets-IDublinCore-title').type(title);
+  cy.get('#select2-chosen-12').click();
+  cy.get('.select2-result-label:last').click();
+  cy.get('ul.select2-choices:first').click();
+  cy.get(`#select2-drop > ul > li:nth-child(${index}) > div`).click();
+  cy.wait(1000);
+  cy.get('#autotoc-item-autotoc-1').click();
+  cy.get('#form-widgets-IShortName-id').type('index.html');
+  cy.saveAndPublish();
+});
