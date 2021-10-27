8a32f49ccce53aa603efa23d34f9d464829e3b87
Petchesi-Iulian <iamnotacat12@gmail.com>
2021-10-27 17:07:21 +0200

    Fix cypress tests.

--- src/collective/taxonomy/javascripts/src/js/components/EditableId.js
+++ src/collective/taxonomy/javascripts/src/js/components/EditableId.js
@@ -35,6 +35,7 @@ const EditableId = ({
         language={language}
         name={`${id}`}
         value={`${id}`}
+        className='identifier-input'
         placeholder={intl.formatMessage(messages.editIdentifierLabel)}
         pencil
         onEditDone={(name, newValue) =>
