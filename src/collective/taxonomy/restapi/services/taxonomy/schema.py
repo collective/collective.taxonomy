2452061fe93f8d7cd9c4cb8bb5341249ff0753b2
nileshgulia1 <nileshgulia@gmail.com>
2022-03-18 16:16:35 +0530

    get taxonomySchema

--- src/collective/taxonomy/restapi/services/taxonomy/schema.py
+++ src/collective/taxonomy/restapi/services/taxonomy/schema.py
@@ -1,38 +1,22 @@
 # -*- coding: utf-8 -*-
-from collective.taxonomy.interfaces import ITaxonomy
 from plone.restapi.services import Service
-from collective.taxonomy.interfaces import ITaxonomyControlPanel
 from collective.taxonomy.interfaces import ITaxonomyForm
-from plone.restapi.interfaces import ISerializeToJson
-from plone.restapi.serializer.controlpanels import ControlpanelSerializeToJson
 from plone.restapi.serializer.controlpanels import get_jsonschema_for_controlpanel
 from plone.restapi.types.utils import create_form
-from zope.component import adapter
-from zope.component import getMultiAdapter
-from zope.component import getUtilitiesFor
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
+from plone.restapi.serializer.converters import json_compatible
 
 
-@implementer(ISerializeToJson)
-@adapter(ITaxonomyControlPanel)
+@implementer(IPublishTraverse)
 class TaxonomyGetSchema(Service):
-    def __call__(self, item=None):
-        json = super().__call__()
-        form = create_form(self.controlpanel, self.controlpanel.request, ITaxonomyForm)
+    def reply(self):
+        form = create_form(self.context, self.request, ITaxonomyForm)
         json_schema = get_jsonschema_for_controlpanel(
-            self.controlpanel,
-            self.controlpanel.context,
-            self.controlpanel.request,
+            self,
+            self.context,
+            self.request,
             form=form,
         )
-        json["schema"] = json_schema
-
-        utils = list(getUtilitiesFor(ITaxonomy))
-        json["items"] = [
-            getMultiAdapter((util, self.controlpanel.request), ISerializeToJson)(
-                full_objects=True
-            )
-            for (_name, util) in utils
-        ]
-        return json
+        # Determine required fields
+        return json_compatible(json_schema)
